d04275ffad5aad7d3aff079f5a4774e1405750b2
Paresh <Paresh15singh@gmail.com>
2021-06-11 13:09:18 +0530

    level2 completed

--- app/src/main/java/com/example/examcalculations/Practice_.kt
+++ app/src/main/java/com/example/examcalculations/Practice_.kt
@@ -1,11 +1,47 @@
 package com.example.examcalculations
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Vibrator
+import android.text.Layout
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class Practice_ : AppCompatActivity() {
+
+    lateinit var submit: Button
+    lateinit var velocity: EditText
+    lateinit var lorFac: EditText
+    lateinit var result: TextView
+    lateinit var vib : Vibrator
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_practice_)
+        velocity = findViewById(R.id.velocity)
+        lorFac = findViewById(R.id.lorFac)
+        result = findViewById(R.id.answer)
+        vib = getSystemService(VIBRATOR_SERVICE) as Vibrator
+        val lay: ConstraintLayout = findViewById(R.id.practicelayout)
+        submit = findViewById(R.id.button2)
+
+
+        submit.setOnClickListener {
+            var answer = formula().checkAns(velocity.text.toString().toLong()  )
+            result.text = answer
+            if (answer == lorFac.text.toString()){
+                lay.setBackgroundColor(Color.parseColor("#00ff00"))
+
+            }else{
+                lay.setBackgroundColor(Color.parseColor("#ff0000"))
+
+                @Suppress("DEPRECATION")
+                vib.vibrate(1000)
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/examcalculations/formula.kt
+++ app/src/main/java/com/example/examcalculations/formula.kt
@@ -13,23 +13,14 @@ class formula {
         println(l)
         return l.toString()
     }
-    fun checkAns(v:Long , L:Double) : String{
+    fun checkAns(v:Long ) : String{
 
         val c = 3 * 10.0.pow(8.0)
         val l = 1 / (sqrt(1 + ((v*v)/(c*c))))
         println(l)
         // only first few numbers of l should be equal to L
-        if(l == L){
-            // green background
-            setColor(0,R.drawable.green)
-        }else{
-            setColor(1,R.drawable.red)
-            // red background and vibration
-        }
+
         return l.toString()
     }
 
-    private fun setColor(opt:Int , Color : Int ) {
-        TODO("Not yet implemented")
-    }
 }
